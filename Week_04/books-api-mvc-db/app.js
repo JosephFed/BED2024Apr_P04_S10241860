ba78ac36789218220c72233ba8a3e432d0062fe8
JosephFed <S10241860@connect.np.edu.sg>
2024-07-15 11:48:41 +0800

    Completed week 13 practical

--- Week_04/books-api-mvc-db/app.js
+++ Week_04/books-api-mvc-db/app.js
@@ -1,6 +1,5 @@
 const express = require("express");
 const booksController = require("./controllers/booksController");
-const usersController = require("./controllers/usersController");
 const sql = require("mssql");
 const dbConfig = require("./dbConfig");
 const bodyParser = require("body-parser"); // Import body-parser
@@ -9,12 +8,9 @@ const validateBook = require("./middlewares/validateBook");
 const app = express();
 const port = process.env.PORT || 3000; // Use environment variable or default port
 
-const staticMiddleware = express.static("public"); // Path to the public folder
-
 // Include body-parser middleware to handle JSON data
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true })); // For form data handling
-app.use(staticMiddleware); // Mount the static middleware
 
 // Routes for GET requests (replace with appropriate routes for update and delete later)
 app.get("/books", booksController.getAllBooks);
@@ -22,14 +18,6 @@ app.get("/books/:id", booksController.getBookById);
 app.post("/books", validateBook, booksController.createBook); // POST for creating books (can handle JSON data)
 app.put("/books/:id", validateBook, booksController.updateBook); // PUT for updating books
 app.delete("/books/:id", booksController.deleteBook); // DELETE for deleting books
-
-app.get("/users/search", usersController.searchUsers); //Search user
-app.get("/users/with-books", usersController.getUsersWithBooks);
-app.post("/users", usersController.createUser); // Create user
-app.get("/users", usersController.getAllUsers); // Get all users
-app.get("/users/:id", usersController.getUserById); // Get user by ID
-app.put("/users/:id", usersController.updateUser); // Update user
-app.delete("/users/:id", usersController.deleteUser); // Delete user
 
 app.listen(port, async () => {
   try {
